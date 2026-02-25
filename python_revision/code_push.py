1b5db4806e7daf474f332c21a9c1308713e50109
stellar-tarun <stellartarun@gmail.com>
2026-02-25 07:11:49 +0530

    add python revision code

---
+++ python_revision/code_push.py
@@ -0,0 +1,15 @@
+##how to push code
+## git config --global user.email "youremail@gmail.com"
+## git config --global user.name "YourName"
+
+##now initialize the repo
+## git init
+## git remote add origin https://github.com/yourusername/your-repo-name.git
+
+##if switching to another repo
+## git remote set-url origin https://github.com/yourusername/new-repo-name.git
+
+##Every time you want to push code, just run these 3 commands:
+## git add .
+## git commit -m "added tuple practice code"
+## git push origin main
